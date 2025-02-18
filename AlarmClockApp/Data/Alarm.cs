7e61c493c33a908e149c8df1f1ae55a52b815dae
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2025-02-17 19:33:22 -0600

    Add project files.

---
+++ AlarmClockApp/Data/Alarm.cs
@@ -0,0 +1,16 @@
+﻿namespace AlarmClockApp.Data
+{
+    public class Alarm
+    {
+        public DateTime currrentTime { get; set; }
+        public TimeOnly alarmTime { get; set; }
+        public string alarmTimeString { get; set; }
+        public bool alarmTimeAmPm { get; set; } = true; // true == am, false == pm
+
+
+
+        public Alarm() { }
+
+
+    }
+}
