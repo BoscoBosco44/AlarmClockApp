31e11c24fd8f95e3521b3c883b8ad6ca2250f5d1
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2025-03-21 23:49:16 -0500

    building components

--- AlarmClockApp/Data/Alarm.cs
+++ AlarmClockApp/Data/Alarm.cs
@@ -6,7 +6,8 @@
         public TimeOnly alarmTime { get; set; }
         public string alarmTimeString { get; set; }
         public bool alarmTimeAmPm { get; set; } = true; // true == am, false == pm
-
+        public string timeRemainingStirng { get; set; }
+        public DateTime timeRemaining { get; set; }
 
 
         public Alarm() { }
